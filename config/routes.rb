a8b79e63a86bf85ee299510243d9b3dc3d5e37e8
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 21:35:50 -0800

    Created welcome and sessions controller files

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
+  get '/dashboard', to: 'welcome#dashboard'
+
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
