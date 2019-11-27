3ae8fc5577b4e9b6aa6d1eece1897711ec5e7e6e
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 23:32:01 -0800

    Set up controller actions for users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   has_many :posts
 
   validates :name,
-    # presence: { message: "Name cannot be left blank." },
+    presence: { message: "Name cannot be left blank." },
     format: { with: /\A[a-zA-Z\s]+\z/, message: "Name can only contain letters." },
     length: { minimum: 2, message: "Name must have at least 2 characters." }
 
   validates :email,
-    # presence: { message: "Email cannot be left blank." },
+    presence: { message: "Email cannot be left blank." },
     uniqueness: { case_sensitive: false, message: "An account has already been created with that email address. Please login instead." }
 
   validates :username,
-    # presence: { message: "Username cannot be left blank." },
+    presence: { message: "Username cannot be left blank." },
     length: { minimum: 4, message: "Username must have at least 4 characters." },
     format: { with: /\A[\w]+\z/, message: "Username can only contain letters, numbers, and underscores." },
     uniqueness: { case_sensitive: false, message: "That username is already taken. Please enter a new one." }
