18c8b4832ee7cb5c7264a5b47b078f93967c38f7
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 21:20:22 -0800

    Added routes for root, login, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :categories
   resources :posts
   resources :users
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
