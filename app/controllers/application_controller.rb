cd50a813966646de28b4c6ae8bfef78d9922b1d4
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-27 00:49:39 -0800

    Added private methods in ApplicationController to check if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  private 
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def redirect_to_login
+    if !logged_in? || current_user.blank?
+      redirect_to dashboard_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def new 
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sessions[:user_id] = @user.id 
+      redirect_to 
+    else
+    end
   end
 
   def show
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
   end
 end
