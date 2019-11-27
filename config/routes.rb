d52a933c80f0481bb28086b41448fbd8c151c85c
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 21:40:02 -0800

    Created route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
