f0b2042d71c5eff0430ce2cf98ad14a80c0cc139
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 20:28:24 -0800

    Created migrations/models/controllers/routes for User, Post, and Category

---
+++ db/migrate/20191127042447_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.string :url
+      t.integer :user_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
