7b5491e563408ff369f3a11ad3560282441277a0
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-26 21:03:27 -0800

    Added validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :posts
+
+  validates :name,
+  presence: { message: "Name cannot be left blank." },
+  format: { with: /\A[a-zA-Z\s]+\z/, message: "Name can only contain letters." },
+  length: { minimum: 2, message: "Name must have at least 2 characters." }
+
+  validates :email,
+    presence: { message: "Email cannot be left blank." },
+    uniqueness: { case_sensitive: false, message: "An account has already been created with that email address. Please login instead." }
+
+  validates :username,
+    presence: { message: "Username cannot be left blank." },
+    length: { minimum: 4, message: "Username must have at least 4 characters." },
+    format: { with: /\A[\w]+\z/, message: "Username can only contain letters, numbers, and underscores." },
+    uniqueness: { case_sensitive: false, message: "That username is already taken. Please enter a new one." }
+
+  validates :password,
+    presence: { message: "Password cannot be left blank." },
+    length: { in: 6..20, message: "Password must be between 6-20 characters long." }
 end
