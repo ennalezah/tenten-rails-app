0ae6bcb3329765bf81f634313f7e638f418ddbbf
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-11-30 03:48:45 -0800

    Added controller actions and form for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  def new 
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      render 'new'
+    end
   end
 
   def destroy 
+    session.delete :user_id
+    redirect_to root_path
   end
 end
